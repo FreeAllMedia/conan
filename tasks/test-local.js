f5bfa95e4367ed97022c35be053e6d688031c98f
nicosommi <nicosommi@gmail.com>
2015-12-21 18:41:39 -0300

    spaces to tabs

--- tasks/test-local.js
+++ tasks/test-local.js
@@ -7,14 +7,14 @@ import chai from "chai";
 chai.should(); // This enables should-style syntax
 
 gulp.task("test-local", ["build"], (cb) => {
-  gulp.src(paths.source.lib)
-    .pipe(istanbul()) // Covering files
-    .pipe(istanbul.hookRequire()) // Force `require` to return covered files
-    .on("finish", () => {
-      gulp.src(paths.build.sourceSpec)
-        .pipe(mocha())
-        .pipe(istanbul.writeReports({dir: `${__dirname}/../`, reporters: ["text-summary", "lcovonly"]})) // Creating the reports after tests ran
+	gulp.src(paths.source.lib)
+		.pipe(istanbul()) // Covering files
+		.pipe(istanbul.hookRequire()) // Force `require` to return covered files
+		.on("finish", () => {
+			gulp.src(paths.build.sourceSpec)
+				.pipe(mocha())
+				.pipe(istanbul.writeReports({dir: `${__dirname}/../`, reporters: ["text-summary", "lcovonly"]})) // Creating the reports after tests ran
 				// .pipe(istanbul.enforceThresholds({ thresholds: { global: 100 } })) // Enforce a coverage of 100%
-        .on("end", cb);
-    });
+				.on("end", cb);
+		});
 });
