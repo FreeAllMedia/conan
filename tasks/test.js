04197c56ce72692cac6a8447443f2ecd69a2856d
D. C. Rockwell <dcautomates@gmail.com>
2016-01-14 16:47:00 -0700

    explicit check for 4.0 or 4.1

--- tasks/test.js
+++ tasks/test.js
@@ -1,9 +1,13 @@
 import gulp from "gulp";
 import runSequence from "gulp-run-sequence";
-import compareVersion from "compare-version";
 
 gulp.task("test", cb => {
-	let es6Environment = compareVersion(process.version, "4.0.0") >= 0;	
+	let es6Environment = true;
+
+	if (process.env.TRAVIS_NODE_VERSION) {
+		console.log("TRAVIS_NODE_VERSION", process.env.TRAVIS_NODE_VERSION);
+		es6Environment = ["4.0", "4.1"].indexOf(process.env.TRAVIS_NODE_VERSION) !== -1;
+	}
 
 	if (es6Environment) {
 		runSequence("test-es5", cb);
