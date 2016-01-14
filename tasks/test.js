e9cac4a0e6ca9d3b2af9c00e5661e7d9137b3083
D. C. Rockwell <dcautomates@gmail.com>
2016-01-14 16:19:06 -0700

    correction for es6 detection

--- tasks/test.js
+++ tasks/test.js
@@ -3,7 +3,7 @@ import runSequence from "gulp-run-sequence";
 import compareVersion from "compare-version";
 
 gulp.task("test", cb => {
-	let es6Environment = compareVersion("4.0.0", process.version) >= 0;	
+	let es6Environment = compareVersion(process.version, "4.0.0") >= 0;	
 
 	if (es6Environment) {
 		runSequence("test-es5", cb);
