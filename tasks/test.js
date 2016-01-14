dab517feb2fb96f45b70d1052ec5d8df69ff90fc
D. C. Rockwell <dcautomates@gmail.com>
2016-01-14 16:13:55 -0700

    Added checks for node version number with travis-ci

--- tasks/test.js
+++ tasks/test.js
@@ -1,3 +1,13 @@
 import gulp from "gulp";
+import runSequence from "gulp-run-sequence";
+import compareVersion from "compare-version";
 
-gulp.task("test", ["test-local"]);
+gulp.task("test", cb => {
+	let es6Environment = compareVersion("4.0.0", process.version) >= 0;	
+
+	if (es6Environment) {
+		runSequence("test-es5", cb);
+	} else {
+		runSequence("test-es6", cb);
+	}
+});
