f73a0d5dbf73d2d6c84be2aed26aa030b82cb096
D. C. Rockwell <dcautomates@gmail.com>
2016-01-14 17:34:48 -0700

    Since node4 doesnt support es6 completely, were only going to test es5 in CI until a version of node does.

--- tasks/test.js
+++ tasks/test.js
@@ -1,14 +1,3 @@
 import gulp from "gulp";
-import runSequence from "gulp-run-sequence";
-import compareVersion from "compare-version";
 
-gulp.task("test", cb => {
-	const versionNumber = process.env.TRAVIS_NODE_VERSION || process.version;
-	console.log("VERSION NUMBER:", versionNumber);
-
-	if (compareVersion(versionNumber, "4.0.0") >= 0) {
-		runSequence("test-es6", cb);
-	} else {
-		runSequence("test-es5", cb);
-	}
-});
+gulp.task("test", ["test-es5"]);
